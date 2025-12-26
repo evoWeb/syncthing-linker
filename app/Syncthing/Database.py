804b1e1834a8ce067ce5b765da492a5b1bd18ba5
Sebastian Fischer <typo3@evoweb.de>
2025-12-26 22:28:53 +0100

    [TASK] Update Database class

--- app/Syncthing/Database.py
+++ app/Syncthing/Database.py
@@ -11,12 +11,14 @@ class Database(BaseAPI):
     prefix = '/rest/db/'
 
     def browse(self, folder, levels=None, prefix=None) -> dict:
-        """ Returns the directory tree of the global model.
+        """ Returns the directory tree of the global model. Directories are always JSON objects
+            (map/dictionary), and files are always arrays of modification time and size. The first
+            integer is the files modification time, and the second integer is the file size.
 
-            Directories are always JSON objects (map/dictionary), and files are
-            always arrays of modification time and size. The first integer is
-            the files modification time, and the second integer is the file
-            size.
+            The call takes one mandatory folder parameter and two optional parameters. Optional
+            parameter levels define how deep within the tree we want to dwell down (0-based,
+            defaults to unlimited depth) Optional parameter prefix defines a prefix within the
+            tree where to start building the structure.
 
             Args:
                 folder (str): The root folder to traverse.
@@ -33,7 +35,17 @@ class Database(BaseAPI):
         return self.get('browse', params={'folder': folder, 'levels': levels, 'prefix': prefix})
 
     def completion(self, device, folder) -> int:
-        """ Returns the completion percentage (0 to 100) for a given device and folder.
+        """ Returns the completion percentage (0 to 100) and byte / item counts. Takes optional
+            device and folder parameters:
+
+            - folder specifies the folder ID to calculate completion for. An empty or absent folder
+              parameter means all folders as an aggregate.
+
+            - device specifies the device ID to calculate completion for. An empty or absent device
+              parameter means the local device.
+
+            If a device is specified but no folder, completion is calculated for all folders shared
+            with that device.
 
             Args:
                 device (str): The Syncthing device the folder is syncing to.
@@ -45,7 +57,7 @@ class Database(BaseAPI):
         return self.get('completion', params={'folder': folder, 'device': device}).get('completion', None)
 
     def file(self, folder, file_) -> dict:
-        """ Returns most data available about a given file, including version and availability.
+        """ Returns most data about a given file, including version and availability.
 
             Args:
                 folder (str):
@@ -57,11 +69,10 @@ class Database(BaseAPI):
         return self.get('file', params={'folder': folder, 'file': file_})
 
     def ignores(self, folder) -> dict:
-        """ Returns the content of the ``.stignore`` as the ignored field. A
-            second field, expanded, provides a list of strings which represent
-            globbing patterns described by gobwas/glob (based on standard
-            wildcards) that match the patterns in ``.stignore`` and all the
-            includes.
+        """ Takes one parameter, folder, and returns the content of the .stignore as the ignored
+            field. A second field, expanded, provides a list of strings which represent globbing
+            patterns described by gobwas/glob (based on standard wildcards) that match the patterns
+            in .stignore and all the includes.
 
             If appropriate, these globs are prepended by the following modifiers:
             ``!`` to negate the glob, ``(?i)`` to do case-insensitive matching and
@@ -77,7 +88,10 @@ class Database(BaseAPI):
         return self.get('ignores', params={'folder': folder})
 
     def set_ignores(self, folder, *patterns) -> dict:
-        """ Applies ``patterns`` to ``folder``'s ``.stignore`` file.
+        """ Expects a format similar to the output of GET /rest/db/ignores call, but only
+            containing the ignored field (expanded field should be omitted). It takes one
+            parameter, folder, and either updates the content of the .stignore echoing it
+            back as a response, or returns an error.
 
             Args:
                 folder (str):
@@ -91,8 +105,40 @@ class Database(BaseAPI):
         data = {'ignore': list(patterns)}
         return self.post('ignores', params={'folder': folder}, data=data)
 
+    def localchanged(self, folder) -> dict:
+        """ Takes one mandatory parameter, folder, and returns the list of files which were changed
+            locally in a receive-only folder. Thus, they differ from the global state and could be
+            reverted by pulling from remote devices again, see POST /rest/db/revert.
+
+            The results can be paginated using the common pagination parameters.
+
+            Args:
+                folder (str):
+
+            Returns:
+                dict
+        """
+        return self.get('localchanged', params={'folder': folder})
+
     def need(self, folder, page=None, perpage=None) -> dict:
-        """ Returns lists of files which are needed by this device in order for it to become in sync.
+        """ Takes one mandatory parameter, folder, and returns lists of files which are needed by
+            this device in order for it to become in sync.
+
+            The results can be paginated using the common pagination parameters. Pagination happens
+            across the union of all necessary files, that is - across all 3 sections of the
+            response. For example, given the current need state is as follows:
+
+            1 progress has 15 items
+
+            2 queued has 3 items
+
+            3 rests have 12 items
+
+            If you issue a query with page=1 and perpage=10, only the progress section in the
+            response will have 10 items. If you issue a request query with page=2 and perpage=10,
+            a progress section will have the last 5 items, a queued section will have all 3
+            items, and a rest section will have the first 2 items. If you issue a query for
+            page=3 and perpage=10, you will only have the last 10 items of the rest section.
 
             Args:
                 folder (str):
@@ -109,7 +155,9 @@ class Database(BaseAPI):
         return self.get('need', params={'folder': folder, 'page': page, 'perpage': perpage})
 
     def override(self, folder) -> None:
-        """ Request override of a send-only folder.
+        """ Request override of a send-only folder. Override means to make the local version
+            latest, overriding changes made on other devices. This API call does nothing if
+            the folder is not a send-only folder.
 
             Args:
                 folder (str): folder ID.
@@ -131,8 +179,31 @@ class Database(BaseAPI):
         """
         self.post('prio', params={'folder': folder, 'file': file})
 
+    def remoteneed(self, folder, device) -> dict:
+        """ Takes the mandatory parameters folder and device and returns the list of files which
+            are needed by that remote device in order for it to become in sync with the shared folder.
+
+            The results can be paginated using the common pagination parameters.
+
+            Returns:
+                dict
+        """
+        return self.get('remoteneed', params={'folder': folder, 'device': device})
+
+    def revert(self, folder) -> None:
+        """ Request revert of a receive-only folder. Reverting a folder means to undo all local
+            changes. This API call does nothing if the folder is not a receive-only folder.
+
+            Takes the mandatory parameter folder (folder ID).
+        """
+        self.post('revert', params={'folder': folder})
+
     def scan(self, folder, sub=None, delay=None) -> str:
-        """ Request immediate rescan of a folder, or a specific path within a folder.
+        """ Request immediate scan. Takes the optional parameters folder (folder ID), sub (path
+            relative to the folder root) and next (time in seconds). If a folder is omitted or
+             empty, all folders are scanned. If a sub is given, only this path (and children, in
+             case it’s a directory) is scanned. The next argument delays Syncthing’s automated
+             rescan interval for a given number of seconds.
 
             Args:
                 folder (str): Folder ID.
@@ -140,7 +211,7 @@ class Database(BaseAPI):
                     the entire folder is scanned for changes, otherwise only
                     the given path children are scanned.
                 delay (int): Delays Syncthing's automated rescan interval for
-                    a given number of seconds. Called next in Syncthing docs
+                    a given number of seconds. Called ''next'' in Syncthing docs
 
             Returns:
                 str
