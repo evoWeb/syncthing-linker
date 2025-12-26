93223ed93a73ed632a01607edfce2a8b1c21bf7f
Sebastian Fischer <typo3@evoweb.de>
2025-12-26 11:26:15 +0100

    [TASK] Split out Statistics class

---
+++ app/Request/Statistics.py
@@ -0,0 +1,26 @@
+import BaseAPI
+
+class Statistics(BaseAPI):
+    """ HTTP REST endpoint for Statistic calls."""
+
+    prefix = '/rest/stats/'
+
+    def device(self):
+        """ Returns general statistics about devices.
+
+            Currently, it only contains the time the device was last seen.
+
+            Returns:
+                dict
+        """
+        return self.get('device')
+
+    def folder(self):
+        """ Returns general statistics about folders.
+
+            Currently, it contains the last scan time and the last synced file.
+
+            Returns:
+                dict
+        """
+        return self.get('folder')
