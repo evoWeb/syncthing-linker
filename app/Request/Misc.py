632c1e30d3f31f1eec62ddd6286ddd8e2cc780f1
Sebastian Fischer <typo3@evoweb.de>
2025-12-26 11:32:06 +0100

    [TASK] Split out Misc class

---
+++ app/Request/Misc.py
@@ -0,0 +1,93 @@
+import BaseAPI
+
+class Misc(BaseAPI):
+    """ HTTP REST endpoint for Miscelaneous calls."""
+
+    prefix = '/rest/svc/'
+
+    def device_id(self, id_):
+        """ Verifies and formats a device ID. Accepts all currently valid
+        formats (52 or 56 characters with or without separators, upper or lower
+        case, with trivial substitutions). Takes one parameter, id, and returns
+        either a valid device ID in modern format, or an error.
+
+        Args:
+            id_ (str)
+
+        Raises:
+            SyncthingError: when ``id_`` is an invalid length.
+
+        Returns:
+            str
+        """
+        return self.get('deviceid', params={'id': id_}).get('id')
+
+    def language(self):
+        """ Returns a list of canonicalized localization codes, as picked up
+        from the Accept-Language header sent by the browser. By default, this
+        API will return a single element that's empty; however calling
+        :func:`Misc.get` directly with `lang` you can set specific headers to
+        get values back as intended.
+
+        Returns:
+            List[str]
+
+            >>> s = _syncthing()
+            >>> len(s.misc.language())
+            1
+            >>> s.misc.language()[0]
+            ''
+            >>> s.misc.get('lang', headers={'Accept-Language': 'en-us'})
+            ['en-us']
+        """
+        return self.get('lang')
+
+    def random_string(self, length=32):
+        """ Returns a strong random generated string (alphanumeric) of the
+            specified length.
+
+            Args:
+                length (int): default ``32``.
+
+            Returns:
+                str
+
+            >>> s = _syncthing()
+            >>> len(s.misc.random_string())
+            32
+            >>> len(s.misc.random_string(32))
+            32
+            >>> len(s.misc.random_string(1))
+            1
+            >>> len(s.misc.random_string(0))
+            32
+            >>> len(s.misc.random_string(None))
+            32
+            >>> import string
+            >>> all_letters = string.ascii_letters + string.digits
+            >>> all([c in all_letters for c in s.misc.random_string(128)])
+            True
+            >>> all([c in all_letters for c in s.misc.random_string(1024)])
+            True
+        """
+        return self.get(
+            'random/string',
+            params={'length': length}
+        ).get('random', None)
+
+    def report(self):
+        """ Returns the data sent in the anonymous usage report.
+
+            Returns:
+                dict
+
+            >>> s = _syncthing()
+            >>> report = s.misc.report()
+            >>> 'version' in report
+            True
+            >>> 'longVersion' in report
+            True
+            >>> 'syncthing v' in report['longVersion']
+            True
+        """
+        return self.get('report')
