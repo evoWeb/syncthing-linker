89ba6f0ed0a0820b5a30e1bc22b2dfd530d4ea77
Sebastian Fischer <typo3@evoweb.de>
2025-12-27 22:49:35 +0100

    [TASK] Refactor main.py

--- app/main.py
+++ app/main.py
@@ -1,52 +1,97 @@
 #!/usr/bin/env python
 
 import os
-import time
 import yaml
+
+from Syncthing.Syncthing import Syncthing
 from Syncthing.Syncthing import syncthing_factory
+from Syncthing.SyncthingError import SyncthingError
 
-def load_config(config_path = '/config/config.yaml'):
-    """Lädt die Konfiguration aus einer YAML-Datei."""
-    try:
-        with open(config_path, 'r', encoding='utf-8') as file:
-            config_data = yaml.safe_load(file)
-        return config_data
-    except FileNotFoundError:
-        print(f"Fehler: Die Konfigurationsdatei '{config_path}' wurde nicht gefunden.")
-        return None
-    except yaml.YAMLError as yamlError:
-        print(f"Fehler beim Parsen der YAML-Datei: {yamlError}")
-        return None
+class Main:
+    def __init__(self):
+        self.key = self.get_api_key()
+        self.config = self.load_config()
 
-# Konfiguration laden
-config = load_config()
-print(config)
+        filters: list[str] | None = None
+        if self.config.get('filters', '') != '':
+            filters = self.config['filters'].split(',')
 
-key = os.getenv('SYNCTHING_API_KEY', '')
-print(key)
-if key == '':
-    raise Exception('No API key found.')
+        syncthing = syncthing_factory()
+        self.check_connection(syncthing)
 
-s = syncthing_factory()
+        self.loop_events(syncthing, filters)
 
-# name spaced by API endpoints
-s.system.connections()
+    @staticmethod
+    def get_api_key() -> str:
+        key: str = os.getenv('SYNCTHING_API_KEY', '')
+        if key == '':
+            raise Exception('No API key found.')
+        return key
 
-# supports GET/POST semantics
-sync_errors = s.system.errors()
-s.system.clear()
+    @staticmethod
+    def load_config(config_path ='/config/config.yaml') -> dict | None:
+        """ Load configuration and check if minimum requirements are met """
+        try:
+            with open(config_path, 'r', encoding='utf-8') as file:
+                config_data = yaml.safe_load(file)
 
-if sync_errors:
-    for e in sync_errors:
-        print(e)
+            if config_data is None:
+                raise Exception('Configuration is empty.')
 
-#while True:
-    # supports long-polling event
-event_stream = s.events(limit=10)
-for event in event_stream:
-    print(event)
-    # do something with `event`
-    if event_stream.count > 100:
-        event_stream.stop()
-#    time.sleep(10)
-event_stream.stop()
+            if 'source' not in config_data:
+                config_data['source'] = '/files/source/'
+            if 'destination' not in config_data:
+                config_data['destination'] = '/files/destination/'
+            if 'filters' not in config_data:
+                config_data['filters'] = 'ItemFinished'
+
+            return config_data
+        except FileNotFoundError:
+            print(f"Fehler: Die Konfigurationsdatei '{config_path}' wurde nicht gefunden.")
+            return None
+        except yaml.YAMLError as yamlError:
+            print(f"Fehler beim Parsen der YAML-Datei: {yamlError}")
+            return None
+
+    @staticmethod
+    def check_connection(wrapper: Syncthing) -> None:
+        """ Checks the connection to the Syncthing API """
+        wrapper.system.connections()
+
+        # supports GET/POST semantics
+        sync_errors = wrapper.system.errors()
+        wrapper.system.clear()
+
+        if sync_errors:
+            for e in sync_errors:
+                print(e)
+                exit(1)
+
+    def loop_events(self, syncthing: Syncthing, filters) -> None:
+        last_seen_id: int | None = None
+        while True:
+            if last_seen_id:
+                event_stream = syncthing.events(limit=10, filters=filters, last_seen_id=last_seen_id)
+            else:
+                event_stream = syncthing.events(limit=10, filters=filters)
+
+            try:
+                for event in event_stream:
+                    self.process_event(syncthing, event)
+            except SyncthingError:
+                last_seen_id = event_stream.last_seen_id
+            except KeyboardInterrupt:
+                event_stream.stop()
+                print('bye bye')
+                exit(0)
+
+    def process_event(self, syncthing: Syncthing, event) -> None:
+        if event['data']['error'] is str:
+            return
+        file = syncthing.database.file(event['data']['folder'], event['data']['item'])
+        print(syncthing.statistics.folder())
+        print(event)
+        print(file)
+
+if __name__ == '__main__':
+    Main()
