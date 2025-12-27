35db2df1d2900e4ed6d187a12978114a71ce810a
Sebastian Fischer <typo3@evoweb.de>
2025-12-27 15:35:35 +0100

    [TASK] Add Debug class

---
+++ app/Syncthing/Debug.py
@@ -0,0 +1,79 @@
+from .BaseAPI import BaseAPI
+
+class Debug(BaseAPI):
+    """ HTTP REST endpoint for Debug calls.
+
+        Implements endpoints of https://docs.syncthing.net/dev/rest.html#debug-endpoints
+    """
+
+    prefix = '/rest/debug/'
+
+    # DEPRECATED
+    def cpuprof(self) -> dict:
+        """ Used to capture a profile of what Syncthing is doing on the CPU. See Profiling.
+
+            Returns:
+                dict
+
+            >>> s = syncthing_factory().debug
+            >>> debug = s.debug()
+            >>> debug
+            ... #doctest: +ELLIPSIS
+            {...}
+            >>> len(debug.keys())
+            2
+            >>> 'enabled' in debug and 'facilities' in debug
+            True
+            >>> isinstance(debug['enabled'], list) or debug['enabled'] is None
+            True
+            >>> isinstance(debug['facilities'], dict)
+            True
+        """
+        return self.get('cpuprof')
+
+    # DEPRECATED
+    def heapprof(self) -> dict:
+        """ Used to capture a profile of what Syncthing is doing with the heap memory. See Profiling.
+
+            Returns:
+                dict
+
+            >>> s = syncthing_factory().system
+            >>> debug = s.debug()
+            >>> debug
+            ... #doctest: +ELLIPSIS
+            {...}
+            >>> len(debug.keys())
+            2
+            >>> 'enabled' in debug and 'facilities' in debug
+            True
+            >>> isinstance(debug['enabled'], list) or debug['enabled'] is None
+            True
+            >>> isinstance(debug['facilities'], dict)
+            True
+        """
+        return self.get('heapprof')
+
+    def support(self) -> dict:
+        """ Collects information about the running instance for troubleshooting purposes. Returns
+            a “support bundle” as a zipped archive, which should be sent to the developers after
+            verifying it contains no sensitive personal information. Credentials for the web GUI
+            and the API key are automatically redacted already.
+
+            Returns:
+                dict
+        """
+        return self.get('support')
+
+    def file(self, folder: str, file: str) -> dict:
+        """ Shows diagnostics about a certain file in a shared folder. Takes the folder (folder ID)
+            and file (folder relative path) parameters.
+
+            Args:
+                folder (str): folder relative path
+                file (str): folder relative path
+
+            Returns:
+                None
+        """
+        return self.post('file', params={'folder': folder, 'file': file})
