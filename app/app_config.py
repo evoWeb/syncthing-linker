ce4bc51657c4a146b5673edc407b69a3760b2580
Sebastian Fischer <typo3@evoweb.de>
2025-12-30 13:59:45 +0100

    [TASK] Make comparison leaner and not capture missing files

--- app/app_config.py
+++ app/app_config.py
@@ -21,23 +21,18 @@ class AppConfig:
     @classmethod
     def load_from_yaml(cls, config_path: str = '/config/config.yaml') -> AppConfig:
         """ Load configuration and check if minimum requirements are met """
-        try:
-            with open(config_path, 'r', encoding='utf-8') as file:
-                config = yaml.safe_load(file)
+        with open(config_path, 'r', encoding='utf-8') as file:
+            config = yaml.safe_load(file)
 
-            if config is None:
-                raise Exception('Configuration is empty.')
+        if config is None:
+            raise ValueError('Configuration is empty.')
 
-            return AppConfig(
-                source=str(config.get('source', '/files/source/')),
-                destination=str(config.get('destination', '/files/destination/')),
-                filters=str(config.get('filter', 'ItemFinished')).split(','),
-                excludes=re.compile(str(config.get('excludes', '')))
-            )
-        except FileNotFoundError as e:
-            raise Exception(f'Error: The configuration file \'{config_path}\' doesn\'t exists.') from e
-        except yaml.YAMLError as yamlError:
-            raise Exception(f'Error with parsing the YAML-file: \'{config_path}\'') from yamlError
+        return AppConfig(
+            source=str(config.get('source', '/files/source/')),
+            destination=str(config.get('destination', '/files/destination/')),
+            filters=str(config.get('filter', 'ItemFinished')).split(','),
+            excludes=re.compile(str(config.get('excludes', '')))
+        )
 
 __all__ = [
     'AppConfig'
--- app/syncthing/syncthing.py
+++ app/syncthing/syncthing.py
@@ -87,7 +87,7 @@ class Syncthing:
             host=os.getenv('SYNCTHING_HOST', '127.0.0.1'),
             port=os.getenv('SYNCTHING_PORT', 8384),
             timeout=10,
-            is_https=os.getenv('SYNCTHING_HTTPS', 0) == 1,
+            is_https=os.getenv('SYNCTHING_HTTPS', '0').lower() in ('1', 'true', 'yes'),
             ssl_cert_file=os.getenv('SYNCTHING_CERT_FILE')
         )
 
