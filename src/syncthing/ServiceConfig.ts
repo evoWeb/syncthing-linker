80e5e83b823ebb2434f078713739115426745592
Sebastian Fischer <typo3@evoweb.de>
2026-01-07 20:08:37 +0100

    [TASK] convert service_config.py to ServiceConfig.ts

---
+++ src/syncthing/ServiceConfig.ts
@@ -0,0 +1,14 @@
+// in milliseconds
+export const DEFAULT_TIMEOUT = 10000;
+
+export class ServiceConfig {
+    constructor(
+        public apiKey: string,
+        public host: string = 'localhost',
+        public port: number = 8384,
+        public timeout: number = DEFAULT_TIMEOUT,
+        public isHttps: boolean = false,
+        public sslCertFile: string | undefined = undefined,
+    ) {
+    }
+}
